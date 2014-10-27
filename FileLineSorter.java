a92be9b462a988a69fe68f9923788916ff8fd68c
Samuel F. Fletcher <fletchersf@familysearch.org>
2014-10-27 15:17:09 -0600

    Create FileLineSorter.java

---
+++ FileLineSorter.java
@@ -0,0 +1,24 @@
+class FileLineSorter
+{
+public static void main(String args[])
+{
+  LineReader reader = new LineReader();
+  LineWriter writer = new LineWriter();
+  char[] buffer = new char[1000]();
+boolean done = false;
+long count = 0;
+int size = 1000;
+List ll = new LinkedList();
+while (!done, count++) {
+// Use count as the seed so the string will be unique
+String seed = Long.toHexString(count).toUpperCase();
+StringBuilder builder = new StringBuilder();
+for (i = 0; i < 250, i++) {
+// Build up 1000 byte string
+builder.append(seed);
+}
+ll.add(builder.toString());
+System.out.println("Chars: " + (count + 1) * 4);
+}
+}
+}
