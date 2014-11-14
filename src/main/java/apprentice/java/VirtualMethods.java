99bd806e2c38c1e9497285f051b80f5507d844b3
fletchersf <fletchersf@familysearch.org>
2014-11-14 12:18:47 -0700

    Improved virtual method example

--- src/main/java/apprentice/java/VirtualMethods.java
+++ src/main/java/apprentice/java/VirtualMethods.java
@@ -9,29 +9,53 @@ import java.util.List;
 public class VirtualMethods {
 
   private static class Insect {
-    public String getOrder() {
+    public String getTaxonomyKingdom() {
+      return "Animalia";
+    }
+    public String getTaxonomyPhylum() {
+      return "Arthropoda";
+    }
+    public String getTaxonomyClass() {
+      return "Insecta";
+    }
+    public String getTaxonomyOrder() {
+      return "Unknown";
+    }
+    public String getTaxonomyFamily() {
       return "Unknown";
     }
   }
 
   private static class Butterfly extends Insect {
     @Override
-    public String getOrder() {
+    public String getTaxonomyOrder() {
       return "Lepidoptera";
+    }
+    @Override
+    public String getTaxonomyFamily() {
+      return "Butterfly";
     }
   }
 
   private static class Beetle extends Insect {
     @Override
-    public String getOrder() {
+    public String getTaxonomyOrder() {
       return "Coleoptera";
+    }
+    @Override
+    public String getTaxonomyFamily() {
+      return "Beetle";
     }
   }
 
   public static void main(String... args) {
-    List<Insect> classes = Arrays.asList(new Butterfly(), new Beetle(), new Insect());
-    for (Insect myObject : classes) {
-      System.out.println(myObject.getOrder());
+    List<Insect> insects = Arrays.asList(new Butterfly(), new Beetle(), new Insect());
+    for (Insect insect : insects) {
+      System.out.println(insect.getTaxonomyKingdom() + " : "
+          + insect.getTaxonomyPhylum() + " : "
+          + insect.getTaxonomyClass() + " : "
+          + insect.getTaxonomyOrder() + " : "
+          + insect.getTaxonomyFamily());
     }
   }
 
